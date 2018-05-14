e191ff88e68dfd23206447557219e9896a18067a
yiersa <taojike@126.com>
2018-05-14 10:08:29 +0800

    test

--- app/src/main/java/com/tools2/taojike2/ndktest/MainActivity.java
+++ app/src/main/java/com/tools2/taojike2/ndktest/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv = (TextView) findViewById(R.id.text);
+		
         tv.setText("packageName: " + JniTest.getPackname(MainActivity.this));
     }
 }
